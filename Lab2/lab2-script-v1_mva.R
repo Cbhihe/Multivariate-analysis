9000206187d00a8b75bb9cdfa56cc72d0b9f6e99
Cbhihe <cedric.bhihe@gmail.com>
2018-03-14 17:09:04 +0100

    debug lab2 script v1

--- Lab2/lab2-script-v1_mva.R
+++ Lab2/lab2-script-v1_mva.R
@@ -44,71 +44,78 @@ russet_data = read.csv(paste0("Data/",list_files[1]),
                        sep=",", 
                        check.names=TRUE)
 dim(russet_data)  # list nbr of observations followed by nbr of vars
-X <- russet_data
+
+# initialize matrices
+X <- russet_data ; rm(russet_data)
+X_ctd <- matrix(,nrow(X),ncol(X))
+X_std <- X_ctd
 
 #############################################
 # function for PCA analysis
 #############################################
 
-pcaF <- function(X,wflag,wparam, ...) {
+pcaF <- function(X,wflag,...) {
 
   # "wflag" is in c("random","uniform","arbitrary")
   # if arg "wflag" is:
   #   <> "random"
   #      default weight distribution is random uniform 
   #      over interval [0,1], normalized to 1
+  #      arg "wparam" is ignored 
   #   <> "uniform"
   #      arg "wparam" is ignored 
   #   <> "arbitrary"
   #      weight distribution is given by vector "wparam"
   #      e.g. c(1,2,3,4,5,6,123), normalized to 1.
 
-  if (wflag == "random") {  
+  if (wflag == "random") { 
+    print("ok 'random'")
     # generate random weight for each individual
     W <- runif(nrow(X))
     # normalize weights to 1
     W <- W/sum(W)
-    N <- diag(W,nrow(X),nrow(X))
-    sum(diag(N))  # check that matrix trace = 1
-    
+     
   } else if (wflag == "uniform") {
     # generate uniform weights distribution for each individual
-    N <- diag(1/nrow(X),nrow(X),nrow(X))
-    sum(diag(N))  # check that matrix trace = 1
-    
+    W=rep(1,nrow(X))
+
   } else if (wflag == "arbitrary") {
-   try(if(length(wparam) != nrow(X) | !is.numeric(wparam)) 
-     stop("WARNING: invalid individual weights given to function \"pcaF\".\n
-          Program abort.\n"))
-    N <- diag(wparam/sum(wparam),nrow(X),nrow(X))
+    try(if(length(wparam) != nrow(X) | !is.numeric(wparam)) 
+        stop("WARNING: invalid individual weights given to function 'pcaF'. 
+        Program abort."))
+    W <- wparam
     
   } else {
-        stop("WARNING: invalid parameter \"wflag\" given to function \"pcaF\".\n
-             Program abort.\n")
-      }
-
+    stop("WARNING: invalid parameter \"wflag\" given to function 'pcaF'.
+         Program abort.")
+  }
+  
+  N <- diag(W/sum(W),nrow(X),nrow(X))
+  try(if(sum(diag(N)) != 1) 
+    stop("WARNING: invalid individual weight normalization"))  # check that matrix trace = 1
+  
   # centroid G of individuals.
   (centroid <- colMeans((X)))
   
   # centered X matrix.
-  X_ctd <- X - matrix(rep(centroid, nrow(X)), ncol=9, byrow=T)
+  X_ctd <- X - matrix(rep(t(centroid), nrow(X)), ncol=9, byrow=T)
   rownames(X_ctd) <- rownames(X)
-    # standardized X matrix
-  for (cc in 1:ncol(X)) {
-    X_std[,cc] <- X_ctd[,cc]/sd(X[,cc])  
-    }
+
+  # standardized X matrix
+  for (cc in 1:ncol(X)) X_std[,cc] <- X_ctd[,cc]/sd(X_ctd[,cc])
   rownames(X_std) <- rownames(X)
   
-    # covariance matrix (on) X_ctd) 
+  # covariance matrix (on) X_ctd) 
 
   
-    # correlation matrix (on X_std)
+  # correlation matrix (on X_std)
   
   
   
   
   return
 } #  function closure
+pcaF(X,"random")
 
 #X_cov <- cov(X)
 X_cor <- cor(X)

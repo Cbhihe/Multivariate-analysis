b3c523940444654e556adc55eb64e395233090d4
Cbhihe <cedric.bhihe@gmail.com>
2018-06-14 18:32:10 +0200

    final cut Associations rule!

--- Lab8/script-assoc-rules.R
+++ Lab8/script-assoc-rules.R
@@ -1,7 +1,7 @@
 ## ############################################
-##  Lab #8:   Association Rules
+##  Topic:    Association Rules
 ##  Authors:  Cedric Bhihe, Santi Calvo 
-##  Delivery: before 2018.06.__ - 23:55
+##  Date: 2018.06.15
 ##  Script:   script-ar.R
 ## ############################################
 
@@ -9,6 +9,7 @@
 rm(list=ls(all=TRUE))
 
 setwd("~/Documents/Academic/UPC/MIRI/Subjects/MVA_multivariate-analysis/Labs/")
+# setwd("C:/Users/calvo/Desktop/UPC/Courses/Third_semester/MVA/mva-labs/")
 set.seed(932178)
 
 options(scipen=6) # R switches to sci notation above 5 digits on plot axes
@@ -37,7 +38,7 @@ require(graphics, lib.loc="~/R/x86_64-pc-linux-gnu-library/3.5")        # enhanc
 # ############################################
 ## Functions
 # ############################################
-csvSaveF <- function(dataObj,targetfile) {
+csvSaveF <- function(dataObj,targetFile) {
     write.table(dataObj,
                 targetfile,
                 append=F,
@@ -70,12 +71,13 @@ classVar <- lapply(tictt1,class)   # class of each variable
 # ############################################
 
 ## convert every non-factor (logical) variable in a factor
-tictt1[sapply(tictt1,is.logical)] <- lapply(tictt1[sapply(tictt1,is.logical)],as.factor)
-str(tictt1)                       # verify conversion
-length(unique(names(tictt1)))     # check =33
+tictt2 <- tictt1
+tictt2[sapply(tictt1,is.logical)] <- lapply(tictt1[sapply(tictt1,is.logical)],as.factor)
+str(tictt2)                       # verify conversion
+length(unique(names(tictt2)))     # check =33
 
 # inspect data for missings
-aggr(tictt1, 
+aggr(tictt2, 
      numbers=TRUE,
      bars=TRUE,
      combined=FALSE,
@@ -88,7 +90,7 @@ aggr(tictt1,
      ylab=c("Missing Data (count)","In-Variable Missing Data Distribution"))
 
 # lookup profile of people who buy in internet.
-tictt1_catdes <- catdes(tictt1,
+tictt2_catdes <- catdes(tictt2,
                         28,
                         proba=0.05,
                         row.w=NULL)
@@ -97,16 +99,10 @@ tictt1_catdes <- catdes(tictt1,
 # ############################################
 ## 3: Transform `tictt` in a transaction file
 # ############################################
-tictt2 <- read.csv("Data/tic_tt.txt",
-                   header=T,
-                   #encoding="UTF-8",
-                   fileEncoding = "ISO-8859-1",
-                   sep=";")
-rownames(tictt2) <- tictt2[,1]
-tictt2 <- tictt2[,-1]
-ticttr <- as(tictt2,"transactions")    # ensure 'arules' is correctly loaded before
-rm(tictt2)
+ticttr <- as(tictt1,"transactions")    # ensure 'arules' is correctly loaded before
 
+# Inspect the transctions variable
+inspect(head(ticttr, n = 5))
 
 # ############################################
 ## 4: Define itemsets' parameters: min_support, min_confidence, max_size,
@@ -117,5 +113,43 @@ min_support <- min(itemFrequency(ticttr))
 sort(itemFrequency(ticttr), decreasing=F)[1:10]
 length(itemFrequency(ticttr))
 # itemFrequencyPlot(ticttr)
-min_confidence <- min()
-max_size <- max()
+ticTransRules = apriori(ticttr)  # execute with default parameter values:
+                                 # support=.1,confidence=.8,maxlen=10,maxtime=5
+summary(ticTransRules)           # check that the maximum item length is 9
+attributes(ticTransRules)
+class(ticTransRules)
+
+
+# We choose:
+min_support <- .01
+min_confidence <- 0.75
+max_size <- 5
+
+ticTransRules = apriori(ticttr,parameter=list(support=min_support,
+                                              confidence=min_confidence,
+                                              maxlen=max_size,
+                                              maxtime=10))
+summary(ticTransRules)
+inspect(head(ticTransRules, n = 10, by ="confidence",decreasing=F))
+inspect(head(ticTransRules, n = 10, by ="lift",decreasing=T))
+# ticTR <- subset(ticTransRules, subset = lhs != "")   # impossible to apply for object of type "rules"
+
+
+# ############################################
+## 5. List the 10 most frequent item sets (i.e. rules sorted by support)
+# ############################################
+inspect(head(ticTransRules, n = 10, by = "support",decreasing=T))
+
+
+# ############################################
+## 6. List the first 10 rules sorted by lift
+# ############################################
+inspect(head(ticTransRules, n = 10, by ="lift",decreasing=T))
+
+
+# ############################################
+## 7. List the first 10 rules sorted by lift with "Pagament.a.través.d.Internet." as Consequent
+# ############################################
+ticTR <- subset(ticTransRules, subset = rhs %in% c("Pagament.a.través.d.Internet."))
+summary(ticTR)
+inspect(head(ticTR,n = 10, by="lift", decreasing=T))
